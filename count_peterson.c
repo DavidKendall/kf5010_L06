649adeebc2f2066befe7852fa0e6bed3286d315a
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-10 20:42:30 +0100

    LCD ok when separate commands used inside critical section

--- count_peterson.c
+++ count_peterson.c
@@ -50,7 +50,8 @@ void *count1_thr(void * arg) {
         if ((count1 + count2) != total) {
             flashing = true;
         }
-        lcd_write_at(1, 0, "count1 = %20d", count1);
+        lcd_set_pos(1, 0);
+        lcd_write("count1 = %20d", count1);
         need1 = false;
     }
     while (true) {
@@ -71,7 +72,8 @@ void *count2_thr(void * arg) {
         if ((count1 + count2) != total) {
             flashing = true;
         }
-        lcd_write_at(2, 0, "count2 = %20d", count2);
+        lcd_set_pos(2, 0);
+        lcd_write("count2 = %20d", count2);
         need2 = false;
     }
     while (true) {
